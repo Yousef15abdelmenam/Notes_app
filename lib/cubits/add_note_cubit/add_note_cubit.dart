bea84e2b003250f7b75c1f86fa494f19be938d97
Yousef15abdelmenam <yousef15abdelmenam@gmail.com>
2025-02-10 19:25:36 +0200

    refator multi block provider

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -9,20 +9,15 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
-
-
   addNote(NoteModel note) async {
-
     emit(AddNoteLoading());
     try {
-      var notesBox = Hive.box(kNotesBox);
-  
-      emit(AddNoteSuccess());
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
 
       await notesBox.add(note);
+      emit(AddNoteSuccess());
     } catch (e) {
-  
-      AddNoteFail(e.toString());
+      emit(AddNoteFail(e.toString()));
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,10 @@ import 'package:notes_app/simple_bloc_observer.dart';
 import 'package:notes_app/views/notes_view.dart';
 
 void main() async {
-  Bloc.observer =SimpleBlocObserver();
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
-  await Hive.openBox(kNotesBox);
   Hive.registerAdapter(NoteModelAdapter());
+  await Hive.openBox<NoteModel>(kNotesBox);
 
   runApp(NotesApp());
 }
@@ -21,17 +21,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddNoteCubit(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
-        home: NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+      home: NotesView(),
     );
   }
 }
